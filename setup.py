6826503085c25e4014c3f13994a3ff722165ef14
Shotaro Kohama <khmshtr28@gmail.com>
2018-05-04 21:37:01 +0900

    Update long_description in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python
 
+from os import path
+
 from setuptools import find_packages
 from distutils.core import setup
+
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 requires = [
     'gspread'
@@ -19,10 +27,12 @@ classifiers = [
 
 setup(
     name='labgsheet',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
     license='MIT',
     description='A python library to note ml experiments on google sheet',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/shotarok/labgsheet',
     author='Shotaro Kohama',
     author_email='khmshtr28@gmail.com',
